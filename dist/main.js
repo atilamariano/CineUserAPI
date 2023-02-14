566a50463fb51a0073f0fab4deb4e49d8b153113
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-02-13 21:17:27 -0300

    feat: alterada a port
    Signed-off-by: Atila Mariano <103537702+atilamariano@users.noreply.github.com>

--- dist/main.js
+++ dist/main.js
@@ -22,7 +22,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api/docs', app, document);
-    await app.listen(3333);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
